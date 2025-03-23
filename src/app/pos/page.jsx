60ba019873dbf441e4c7c785b2e5384674294d98
sasith duleepa <duleepajayathissa@gmail.com>
2025-03-23 11:23:07 +0530

    pos interface basic done

--- src/app/pos/page.jsx
+++ src/app/pos/page.jsx
@@ -117,7 +117,7 @@ export default function Page() {
               <p className="sub-title">Customer</p>
               <div className="w-full h-[80%] px-10 flex justify-between items-center">
                 <p>Customer :</p>
-                {/* <Select
+                <Select
                   className="w-3/6"
                   options={options}
                   styles={{
@@ -126,7 +126,7 @@ export default function Page() {
                       borderColor: state.isFocused ? "grey" : "red",
                     }),
                   }}
-                /> */}
+                />
                 <button className="search-btn">Find Customer</button>
               </div>
             </div>
@@ -136,30 +136,43 @@ export default function Page() {
               <p className="sub-title">Sale Item</p>
               <div className="w-full h-[80%] px-10 flex justify-between items-center">
                 <p>Barcode :</p>
-                {/* <Select
+                <Select
                   className="w-4/6"
                   options={options}
                   styles={{
                     control: (baseStyles, state) => ({
                       ...baseStyles,
                       borderColor: state.isFocused ? "grey" : "red",
+                      color: "black",
+                    }),
+                    singleValue: (provided, state) => ({
+                      ...provided,
+                      color: state.data.color,
+                      fontSize: state.selectProps.myFontSize,
+                    }),
+                    option: (provided, state) => ({
+                      ...provided,
+                      fontWeight: state.isSelected ? "bold" : "normal",
+                      color: "black",
+                      backgroundColor: state.data.color,
+                      fontSize: state.selectProps.myFontSize,
                     }),
                   }}
-                /> */}
+                />
                 <button className="search-btn">Category</button>
                 <button className="search-btn">Search</button>
               </div>
             </div>
           </div>
-          <div className="h-4/6 bg-blue-700 overflow-y-scroll">
-            <div className="relative overflow-x-auto">
+          <div className="h-4/6 bg-blue-700 overflow-hidden flex flex-col">
+            <div className="relative flex-1 overflow-hidden">
               <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 sticky top-0 z-10 px-2">
                   <tr>
                     <th scope="col" className="px-6 py-3">
                       Product Name
                     </th>
-                    <th scope="col" className="px-6 py-3">
+                    <th scope="col" className="px-4 py-3">
                       Color
                     </th>
                     <th scope="col" className="px-6 py-3">
@@ -170,61 +183,65 @@ export default function Page() {
                     </th>
                   </tr>
                 </thead>
-                <tbody>
-                  {products.map((product, index) => (
-                    <tr
-                      key={index}
-                      className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
-                    >
-                      <th
-                        scope="row"
-                        className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                      >
-                        {product.name}
-                      </th>
-                      <td className="px-6 py-4">{product.color}</td>
-                      <td className="px-6 py-4">{product.category}</td>
-                      <td className="px-6 py-4">{product.price}</td>
-                    </tr>
-                  ))}
-                </tbody>
               </table>
+              <div className="overflow-y-auto h-full">
+                <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                  <tbody>
+                    {products.map((product, index) => (
+                      <tr
+                        key={index}
+                        className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200"
+                      >
+                        <th
+                          scope="row"
+                          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                        >
+                          {product.name}
+                        </th>
+                        <td className="px-6 py-4">{product.color}</td>
+                        <td className="px-6 py-4">{product.category}</td>
+                        <td className="px-6 py-4">{product.price}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
             </div>
           </div>
         </div>
-        <div className="col-span-1 h-screen bg-emerald-400 p-1">
-          <div className="h-1/6 border flex flex-col gap-7">
-            <p>Payment Type</p>
-            <div className="flex justify-around items-center">
-              <button className="search-btn">CASH</button>
-              <button className="search-btn">CREDIT</button>
+        <div className="col-span-1 h-screen bg-[var(--container-bg)] p-1">
+          <div className="border flex flex-col gap-5 py-1 px-1">
+            <p className="label"> Payment Type</p>
+            <div className="flex justify-around items-center ">
+              <button className="btn bg-[#297b10]">CASH</button>
+              <button className="btn bg-[#85556d]">CREDIT</button>
             </div>
           </div>
 
-          <div className="flex flex-col justify-between gap-15">
+          <div className="flex flex-col justify-between gap-10 p-1">
             <div className="flex  justify-between items-center">
-              <p>No. of Items :</p>
-              <p className="w-max border px-1">XXX</p>
+              <p className="label">No. of Items :</p>
+              <p className="label w-max border px-1">XXX</p>
             </div>
 
             <div>
-              <p>Net Total :</p>
-              <p className="border text-right px-1">XXXXX</p>
+              <p className="label">Net Total :</p>
+              <p className="label border text-right px-1">XXXXX</p>
             </div>
 
             <div>
-              <p>Amount :</p>
-              <input className="border w-full text-right px-1" />
+              <p className="label">Amount :</p>
+              <input className="input h-9 w-full" />
             </div>
 
             <div>
-              <p>Change :</p>
-              <p className="border text-right px-1">XXXXX</p>
+              <p className="label">Change :</p>
+              <p className="label border text-right px-1">XXXXX</p>
             </div>
 
-            <div className="flex justify-end gap-2">
-              <p>Print bill</p>
-              <input type="checkbox" />
+            <div className="flex justify-end gap-2 items-center">
+              <p className="text-2xl">Print bill</p>
+              <input type="checkbox" className="size-6" />
             </div>
 
             <button className="bg-blue-800 w-full px-10 py-5">ENTER</button>
